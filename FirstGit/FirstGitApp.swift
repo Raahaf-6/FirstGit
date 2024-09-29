3ac01714ba45c6be33e55b4bdf627931c1c5abd0
Raahaf-6 <raahafyy@gmail.com>
2024-09-29 11:00:34 +0300

    Initial Commit

---
+++ FirstGit/FirstGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstGitApp.swift
+//  FirstGit
+//
+//  Created by Rahaf on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct FirstGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
