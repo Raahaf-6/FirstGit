8ec7842bb5e545a4b71c2449f67503b5db6d3f79
Raahaf-6 <raahafyy@gmail.com>
2024-09-30 09:35:53 +0300

    Test

--- FirstGit/ContentView.swift
+++ FirstGit/ContentView.swift
@@ -9,15 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        
+        
+          ZStack {
+              Color.white
+                  .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+              
+              
+          
+              
+              
+              VStack{
+                  Image("story")
+                      .resizable()
+                      .padding()
+                      .padding()
+                      .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                      .frame(width: 250, height: 250)
+                      .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                      .overlay(
+                      Circle()
+                          .stroke(Color.gray, lineWidth: 3))
+                      .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                  
+                  VStack(alignment:.leading){
+                      Text("For today")
+                          .font(.title)
+                          .fontWeight(.bold)
+                          .foregroundColor(Color.purple)
+                      Text("You never know how far you can go, if only you decide to do. To belibe, to dare, to dream. To stop fearing, to stop hesitating.")
+                          .font(.headline)
+                          .fontWeight(.medium)
+                          
+                   
+                  }
+                  .padding()
+                      
+
+                  
+                  
+              }
+              
+                  
+                  
+              }
+        
+              
+                      
+
+                  
+                  
+              }
+              
+      
+              }
+        
+              
 
 #Preview {
     ContentView()
